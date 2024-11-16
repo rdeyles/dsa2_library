1f81543bbada3f3f45c829260c22a067ca13c247
rdeyles <rd.eyles@gmail.com>
2024-11-16 18:04:13 +0000

    .

--- Testing Ground.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image
-import matplotlib.pyplot as mp
-import numpy as np
-
-img = np.asarray(Image.open('/Users/rhysdavideyles/Documents/CS/Github/dsa2_library/maze-2.png'))
-print(repr(img))
--- UserArray.py
+++
@@ -1,7 +0,0 @@
-def UserArray(list):
-    print("Please provide an ordered list of numbers one at a time.")
-    n=int(input("How many numbers do you wish to add to the list? "))
-    for i in range(0,n):
-        element=int(input("Enter a number: "))
-        list.append(element)
-    return list
